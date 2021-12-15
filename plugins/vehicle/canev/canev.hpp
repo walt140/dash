163cc5233804c8cbc98776e16846eb31b4b5efd3
xdsquared <35746868+xdsquared@users.noreply.github.com>
2021-12-14 19:48:01 -0800

    updated .hpp for symposium demo

--- plugins/vehicle/canev/canev.hpp
+++ plugins/vehicle/canev/canev.hpp
@@ -1,29 +1,34 @@
 #pragma once
-
 #include <QString>
 #include <iostream>
 #include <stdlib.h>
 #include <QByteArray>
 #include <QPushButton>
-#include <QRadioButton>
+#include <QRadioButton> 
+#include <QCanBus>
+#include <QCanBusFrame>
 #include <boost/log/trivial.hpp>
+
 
 
 #include "plugins/vehicle_plugin.hpp"
 #include "app/widgets/climate.hpp"
 #include "app/arbiter.hpp"
 #include "openauto/Service/InputService.hpp"
+#include "canbus/socketcanbus.hpp"
+#include "canbus/ICANBus.hpp"
+#include "canbus/elm327.hpp"
 
 
-#define G37_LOG(severity) BOOST_LOG_TRIVIAL(severity) << "[G37VehiclePlugin]"
+#define G37_LOG(severity) BOOST_LOG_TRIVIAL(severity) << "[G37VehiclePlugin] "
 
 
 class DebugWindow : public QWidget {
     Q_OBJECT
 
     public:
-    
         DebugWindow(Arbiter &arbiter, QWidget *parent = nullptr);
+     
         QLabel* tpmsOne;
         QLabel* tpmsTwo;
         QLabel* tpmsThree;
@@ -39,7 +44,11 @@ class Buttons : public QWidget {
     public:
         Buttons(Arbiter &arbiter, QWidget *parent = nullptr);
         
-          QPushButton *hazardButton;
+        void toggleHazards();
+        void changeIconsDark();
+        void changeIconsLight();
+               
+        QPushButton *hazardButton;
         QPushButton *lightsButton;
         QPushButton *wipersButton;        
         QPushButton *heaterButton;
@@ -50,14 +59,13 @@ class Buttons : public QWidget {
         QPushButton *rabbitButton;
         QPushButton *turtleButton;
         
-        QLabel *hoistLabel;     
-     
+        QLabel *winchLabel;
         
+signals:
+    void toggleHazards(int &count);
         
-        
-public slots:
-   
-void on_hazardButton_toggled(bool checked);
+private slots:
+    void on_hazardButton_toggled(bool checked);
     void on_lightsButton_toggled(bool checked);
     void on_wipersButton_toggled(bool checked);
     void on_heaterButton_toggled(bool checked);
@@ -66,7 +74,12 @@ void on_hazardButton_toggled(bool checked);
     void on_rabbitButton_toggled(bool checked);
     void on_turtleButton_toggled(bool checked);
     
+    void on_speed_select_toggled(bool checked);
+    void toggleCount();
+    
     void on_arrowUpButton_clicked();
+    
+    
         
 };
 
@@ -76,11 +89,15 @@ class canev : public QObject, VehiclePlugin
     Q_PLUGIN_METADATA(IID VehiclePlugin_iid)
     Q_INTERFACES(VehiclePlugin)
 
-    public:
+public:           
     
-        bool init(ICANBus* canbus) override;
-
-    private:
+    ~canev();
+     bool init(ICANBus* canbus) override;
+     
+public slots:
+     void isDark();
+    
+private:
         QList<QWidget *> widgets() override;
 
         bool duelClimate;
@@ -93,8 +110,9 @@ class canev : public QObject, VehiclePlugin
 
 
 
-        Climate *climate;
+        Climate *climate;       //create poninter to reference each class to create a tab
         DebugWindow *debug;
         Buttons *buttons;
         bool engineRunning = false;
 };
+
